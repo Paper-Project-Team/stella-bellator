958e579f62ef01e888402bd8c239b256d5f9b524
Camden McLeod <ccmcleod05@gmail.com>
2021-10-23 13:33:24 -0500

    Added environments, keybind, and save/load player scripts.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    
+    public int level = 2;
+    public int health = 12;
+
+    public void SavePlayer(){
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer(){
+        PlayerData data = SaveSystem.LoadPlayer(this);
+
+        level = data.level;
+        health = data.health;
+
+        Vector2 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        transform.position = position;
+    }
+}
